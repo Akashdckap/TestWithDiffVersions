f41da9e409a4c65f352fafd076aca40174d11804
Akash <akashs@dckap.com>
2024-12-26 20:21:24 +0530

    Routing initializing

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace API1
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //it defines the inital routing and setup for routers
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
