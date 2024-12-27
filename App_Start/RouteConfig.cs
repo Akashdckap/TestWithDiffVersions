f2efff593c32fcd0da844676528d7076a4a6db41
Akash <akashs@dckap.com>
2024-12-27 10:58:10 +0530

    Child branch testing

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace API1
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            //it defines the inital routing and setup for routers check 
+            //Routing Path defined
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
